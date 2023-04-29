30d92c7a8ce86168416b2235f4e53ec5b1870c04
anaselrayan <anas.g.elrayan@gmail.com>
2023-04-29 11:00:00 +0300

    room-chat-feature

---
+++ src/main/java/com/anas/chatty/ChattyApplication.java
@@ -0,0 +1,13 @@
+package com.anas.chatty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anas/chatty/ChattyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anas.chatty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
